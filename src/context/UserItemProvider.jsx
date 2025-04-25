bca812f953c35c35a9a8ac9de07b94b8ae9b9acb
ailraon <aadsf1789@naver.com>
2025-04-25 09:28:38 +0900

    사용자 필터 기능 추가

--- src/context/UserItemProvider.jsx
+++ src/context/UserItemProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useMemo } from "react";
 import UserItemContext from './UserItemContext';
 
 const UserItemProvider = ({ children }) => {
@@ -14,6 +14,7 @@ const UserItemProvider = ({ children }) => {
 
   // userItemList 상태 초기화
   const [userItemList, setUserItemList] = useState(loadUserItemsFromLocalStorage);
+  // const sortedUserItemList = useMemo(() => sortUserItems());
 
   // 로컬 스토리지에 userItemList 값 저장
   const saveUserItemsToLocalStorage = (items) => {
@@ -42,8 +43,44 @@ const UserItemProvider = ({ children }) => {
     return userItem;
   }, [userItemList]);
 
+  // 정렬
+  const sortUserItems = (criteria) => {
+    const sortedItems = [...userItemList]; // 원본을 수정하지 않기 위해 복사본을 만듬
+  
+    switch (criteria) {
+      case 'latest':
+        // 최신순: id 값이 큰 것부터 정렬 (내림차순)
+        sortedItems.sort((a, b) => b.key - a.key);
+        break;
+      case 'oldest':
+        // 오래된 순: id 값이 작은 것부터 정렬 (오름차순)
+        sortedItems.sort((a, b) => a.key - b.key);
+        break;
+      case 'nameAs':
+        // 이름순: 알파벳 순으로 정렬 (오름차순)
+        sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'nameDe':
+        // 이름순: 알파벳 순으로 정렬 (내림차순)
+        sortedItems.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case 'ageAs':
+        // 나이순: 나이가 적은 순으로 정렬 (오름차순)
+        sortedItems.sort((a, b) => a.age - b.age);
+        break;
+      case 'ageDe':
+        // 나이순: 나이가 많은 순으로 정렬 (내림차순)
+        sortedItems.sort((a, b) => b.age - a.age);
+        break;
+      default:
+        console.log("실패함.");
+        break;
+    }
+    return sortedItems;
+  };
+
   return (
-    <UserItemContext.Provider value={{ userItemList, addUserItem, deleteUserItem, getUserItem }}>
+    <UserItemContext.Provider value={{ userItemList, addUserItem, deleteUserItem, getUserItem, sortUserItems }}>
       {children}
     </UserItemContext.Provider>
   );
--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,40 +1,73 @@
-import React, { useContext, useState } from 'react'
-import UserItem from '../components/UserItem';
-import UserItemContext from '../context/UserItemContext';
-import styled from 'styled-components';
+import React, { useContext, useEffect, useMemo, useState } from "react";
+import UserItem from "../components/UserItem";
+import UserItemContext from "../context/UserItemContext";
+import styled from "styled-components";
 
 // 유저 목록 페이지
 // 전체 유저 리스트 보여주기
 // 유저 아이템 클릭시, 유저 상세 페이지로.
 // 유저 등록 버튼 클릭시 유저 등록 페이지로.
 
-// 필요 상태. 유저 정보 리스트. 
+// 필요 상태. 유저 정보 리스트.
 
 const UserListContainer = styled.div`
+  position: relative;
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
   justify-content: center;
   margin-top: 20px;
+  padding-top: 50px;
+`;
+
+const Fillter = styled.div`
+  position: absolute;
+  top: 0;
+  right: 10%;
+  gap: 20px;
 `;
 
 const UserList = () => {
-    const {userItemList} = useContext(UserItemContext);
+  const [fillterOption, setFillterOption] = useState("latest");
+  const { userItemList, sortUserItems } = useContext(UserItemContext);
+
+  const sortUserItemList = useMemo(() => sortUserItems(fillterOption), [fillterOption, userItemList])
+  
+  // useEffect(() => {
+  //   console.log(fillterOption);
+  //   console.log(sortUserItemList);
+  // }, [fillterOption]);
 
   return (
     <>
-        <UserListContainer>
-            {userItemList.length > 0 ? userItemList.map((userItem) => (
-                <UserItem key={userItem.key} id={userItem.key} name={userItem.name} age={userItem.age} status={userItem.isOnline}/>
-            ))
-            :
-              <div>
-                새로운 사용자를 등록해주세요.
-              </div>
-            }
-        </UserListContainer>
+      <UserListContainer>
+        <Fillter>
+          <span>정렬 기준</span>
+          <select style={{marginLeft: "10px"}} onChange={(e) => setFillterOption(e.target.value)}>
+            <option value="latest">최신순</option>
+            <option value="oldest">오래된순</option>
+            <option value="nameAs">이름순 ↑</option>
+            <option value="nameDe">이름순 ↓</option>
+            <option value="ageAs">나이순 ↑</option>
+            <option value="ageDe">나이순 ↓</option>
+          </select>
+        </Fillter>
+        {userItemList.length > 0 ? (
+          sortUserItemList.map((userItem) => (
+            <UserItem
+              key={userItem.key}
+              id={userItem.key}
+              name={userItem.name}
+              age={userItem.age}
+              status={userItem.isOnline}
+            />
+          ))
+        ) : (
+          <div>새로운 사용자를 등록해주세요.</div>
+        )}
+      </UserListContainer>
     </>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
